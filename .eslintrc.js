27b0dc12888519c556b868a7b7c59bbcb9880535
cagdaskalsen <cagdaskalsen@hotmail.com>
2023-03-01 00:58:49 -0500

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,23 @@
+/* eslint-disable eol-last */
 /* eslint-disable quotes */
+/* eslint-disable indent */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "airbnb"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    "prefer-template": 0,
-    "react/no-children-prop": [0],
-    "linebreak-style": 0,
-  },
+    extends: ["plugin:react/recommended", "airbnb"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        "prefer-template": 0,
+        "react/no-children-prop": [0],
+        "linebreak-style": 0,
+    },
 };
